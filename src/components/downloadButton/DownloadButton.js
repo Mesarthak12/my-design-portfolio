725119e191c7070494037382f75adf34ded6849e
Sarthak Jain <mesarthak.jain19@gmail.com>
2025-05-20 21:54:24 +0530

    fixed the issue for downloading resume

--- src/components/downloadButton/DownloadButton.js
+++ src/components/downloadButton/DownloadButton.js
@@ -3,7 +3,10 @@ import React from "react";
 const DownloadButton = () => {
   return (
     <div>
-      <a href="/cindrella_dsouza_CV.pdf" download="My_Resume.pdf">
+      <a
+        href="/cindrella dsouza resume.pdf"
+        download="Cindrella_Dsouza_Resume.pdf"
+      >
         <button className="text-white w-[169px] h-[64px] cursor-pointer !bg-[#03045E] rounded-lg transition duration-300 ease-in-out transform hover:scale-110">
           <h1 className="font-medium text-xl font-poppins ">Download CV</h1>
         </button>
